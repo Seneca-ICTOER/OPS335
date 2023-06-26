baef08c279be755c1ea9ef6089be11e5b62fd33b
Chris Johnson <chris.johnson@senecacollege.ca>
2023-06-26 12:38:13 -0400

    Initial reconfig from template.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -5,19 +5,19 @@ const katex = require('rehype-katex');
 
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
-  title: 'OER Course Title',
-  tagline: 'OER Tag/Keyword',
+  title: 'OPS335 - Open System Application Server',
+  tagline: 'OPS335',
   url: 'https://Seneca-ICTOER.github.io/',
-  baseUrl: '/OERTemplate/',
+  baseUrl: '/OPS335/',
   trailingSlash: false,
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
   organizationName: 'Seneca-ICTOER',
-  projectName: 'OERTemplate',
+  projectName: 'OPS335',
   themeConfig: {
     navbar: {
-      title: 'OER Course Title',
+      title: 'OPS335 - Open System Application Server',
       logo: {
         alt: 'Seneca College',
         src: 'img/logo.svg',
@@ -31,7 +31,7 @@ module.exports = {
       style: 'dark',
       links: [
         {
-          title: 'OER Course Title',
+          title: 'OPS335 - Open System Application Server',
           items: [
             {
               label: 'Contents',
@@ -57,7 +57,7 @@ module.exports = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           routeBasePath: '/',
-          editUrl: 'https://github.com/Seneca-ICTOER/OERTemplate/tree/main',
+          editUrl: 'https://github.com/Seneca-ICTOER/OPS335/tree/main',
           remarkPlugins: [math],
           rehypePlugins: [katex],
         },
